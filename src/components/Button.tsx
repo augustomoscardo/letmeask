e1b9e285c50aad7dd96f2ce3e1dd93539f018d81
Augusto Moscardo <augustomsaluz@gmail.com>
2021-06-23 23:33:02 -0300

    Dia 4

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,16 @@ import { ButtonHTMLAttributes } from "react"
 
 import '../styles/button.scss'
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> /* passar para o elemento do botão do HMTL toda tipagem que um botão pode receber por meio de ButtonHTMLAttributes, 
-  a tipagem do elemento button é global*/
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isOutlined?: boolean
+}
 
-export function Button(props: ButtonProps) {
+export function Button({ isOutlined = false, ...props }: ButtonProps) {
 
   return (
-    <button className="button" {...props} /> 
+    <button 
+    className={`button ${isOutlined ? 'outlined' : ''}`} 
+    {...props} /> 
 
   )
 }
