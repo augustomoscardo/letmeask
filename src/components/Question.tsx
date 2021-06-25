d9f0ae35002231c4b713e44c29c8edcc96a92d7c
Augusto Moscardo <augustomsaluz@gmail.com>
2021-06-25 15:42:12 -0300

    Dia 5

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,18 +1,28 @@
 import { ReactNode } from "react"
 
+import cx from 'classnames'
+
 type QuestionProps = {
   content: string;
   author: {
     name: string;
     avatar: string;
   }
-  children?: ReactNode
+  children?: ReactNode;
+  isAnswered?: boolean;
+  isHighLighted?: boolean;
 }
 
-export function Question({ content, author, children }: QuestionProps) {
+export function Question({ content, author, children, isAnswered = false, isHighLighted = false }: QuestionProps) {
   
   return (
-    <div className="question">
+    <div 
+      className={cx(
+        'question',
+        { answered: isAnswered },
+        { highlighted: isHighLighted && !isAnswered }
+      )}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -59,8 +59,6 @@ export function Room() {
         authorId: user?.id,
       })
     }
-
-
   }
 
   return (
@@ -104,8 +102,11 @@ export function Room() {
                 key={question.id} 
                 content={question.content}
                 author={question.author}
+                isAnswered={question.isAnswered}
+                isHighLighted={question.isHighLighted}
               >
-                <button
+                {!question.isAnswered && (
+                  <button
                   className={`like-button ${question.likeId ? 'liked' : ''}`}
                   type="button"
                   aria-label="Marcar como gostei"
@@ -117,6 +118,7 @@ export function Room() {
                   </svg>
 
                 </button>
+                )}
               </Question>
             )
           })}
